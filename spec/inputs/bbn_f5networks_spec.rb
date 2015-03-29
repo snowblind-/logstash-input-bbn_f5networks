55e53648e042e04e841e79ba58f9335623bcf4bf
Baffin Bay Networks <devops-github@baffinbaynetworks.com>
2015-03-30 00:12:01 +0200

    Create bbn_f5networks_spec.rb

---
+++ spec/inputs/bbn_f5networks_spec.rb
@@ -0,0 +1,8 @@
+##########################################################################################
+# Copyright (C) 2015 Buffin Bay Networks, Inc - All Rights Reserved
+# Unauthorized copying of this file, via any medium is strictly prohibited
+# Proprietary and confidential
+# Written by Devops <devops-github@baffinbaynetworks.com>, March 2015
+##########################################################################################
+
+require "logstash/devutils/rspec/spec_helper"
