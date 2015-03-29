f4ddee3e82bd8a3b9dc05ce58f0234db4e2e4cd5
Baffin Bay Networks <devops-github@baffinbaynetworks.com>
2015-03-30 00:18:03 +0200

    Update bbn_f5networks.rb

--- lib/logstash/inputs/bbn_f5networks.rb
+++ lib/logstash/inputs/bbn_f5networks.rb
@@ -1,3 +1,9 @@
+##########################################################################################
+# Copyright (C) 2015 Buffin Bay Networks, Inc - All Rights Reserved
+# Unauthorized copying of this file, via any medium is strictly prohibited
+# Proprietary and confidential
+# Written by Devops <devops-github@baffinbaynetworks.com>, March 2015
+##########################################################################################
 
 # Logstash specific dependencies
 require "logstash/inputs/base"
